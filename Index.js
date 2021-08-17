0516db6df42c5e4182517f9db9dee658d4044700
Robert Mendoza <83177653+Bobisblack19@users.noreply.github.com>
2021-08-17 13:49:11 -0400

    Update Index.js
    Added buttons that allowed the timer to start, stop and reset.

--- Index.js
+++ Index.js
@@ -4,6 +4,10 @@ import './style.css';
 
 let counter = 0;
 
+const button = document.getElementById('reset');
+
+const btn = document.getElementById('start');
+
 function show() {
   counter++;
   const el = <p>{counter}</p>;
@@ -12,4 +16,21 @@ function show() {
   );
 }
 
-setInterval(show, 1000); 
+var myVar; 
+
+function myFunction() {
+  myVar = setInterval(show, 1000);
+}
+
+btn.onclick = function() {myFunction()}
+
+function reset() {
+  clearInterval(myVar);
+  counter = counter - counter;
+  const el = <p>{counter}</p>;
+  ReactDOM.render(
+    el, document.getElementById('root')
+  );
+};
+
+button.onclick = function() {reset()};
